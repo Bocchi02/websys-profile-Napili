7239ae0b97d5856420581e1feafb1e7a2fe4223c
CoinciDenz <113191768+CoinciDenz@users.noreply.github.com>
2023-05-12 23:58:15 +0800

    add about title & updated app stylesheet

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const About = () => {
-    return <div>About</div>;
+    return (
+        <div className="section container">
+            <section className="about">
+                <h2 className="section__title">
+                    About <span>Me</span>
+                </h2>
+            </section>
+        </div>
+    );
 };
 
 export default About;
