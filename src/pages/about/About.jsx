bdfffd32da8750044928f46c9015803d40542a6f
CoinciDenz <113191768+CoinciDenz@users.noreply.github.com>
2023-05-13 00:24:22 +0800

    updated about.jsx

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -7,6 +7,13 @@ const About = () => {
                 <h2 className="section__title">
                     About <span>Me</span>
                 </h2>
+
+                <div className="about__container grid">
+                    <div className="about__info">
+                        <h3 className="section__subtitle">Personal Info</h3>
+                        <ul className="info__list grid"></ul>
+                    </div>
+                </div>
             </section>
         </div>
     );
