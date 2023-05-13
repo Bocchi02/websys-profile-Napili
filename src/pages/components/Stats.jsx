02fc3dfb0fe8e6ebdb34a88bd137d0908967ac34
CoinciDenz <113191768+CoinciDenz@users.noreply.github.com>
2023-05-13 13:05:52 +0800

    updated Stats.jsx

--- src/pages/components/Stats.jsx
+++ src/pages/components/Stats.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { stats } from "../../data";
 
 const Stats = () => {
-    return <div>Stats</div>;
+    return (
+        <>
+            {stats.map(({ no, title }, index) => {
+                return (
+                    <div className="stats__box" key={index}>
+                        <h3 className="stats__no">{no}</h3>
+                        <p className="stats__title">{title}</p>
+                    </div>
+                );
+            })}
+        </>
+    );
 };
 
 export default Stats;
